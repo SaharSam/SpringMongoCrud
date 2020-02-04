f634d16b8f66f1420e88933361adc0978fe34d4e
SaharSam <52576006+SaharSam@users.noreply.github.com>
2020-02-05 00:09:34 +0330

    First Commit

---
+++ src/main/java/me/sahar/springmongocrud/SpringmongocrudApplication.java
@@ -0,0 +1,13 @@
+package me.sahar.springmongocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongocrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmongocrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/sahar/springmongocrud/SpringmongocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sahar.springmongocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongocrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
